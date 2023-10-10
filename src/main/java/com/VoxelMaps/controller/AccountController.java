8d3659b8777942807c5b8eb4e9ee134e28800d9a
a.burgart02 <a.burgart02@gmail.com>
2023-10-10 21:59:39 +0500

    Add account page

--- src/main/java/com/VoxelMaps/controller/AccountController.java
+++ src/main/java/com/VoxelMaps/controller/AccountController.java
@@ -1,13 +1,17 @@
 package com.VoxelMaps.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AccountController
 {
     @RequestMapping("/account")
-    public String getMainPage() {
-        return "test";
+    public String getMainPage(ModelMap model, HttpServletRequest request) {
+        String user=request.getUserPrincipal().getName();
+        model.addAttribute("username", user);
+        return "account";
     }
 }
